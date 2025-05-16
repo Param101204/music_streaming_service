5c4bfb2bc1c5b2a24f92d6d2bfd6b92c84703fdb
Param101204 <param1012daiict@gmail.com>
2025-05-16 21:06:43 +0530

    db.js

--- models/db.js
+++ models/db.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://db.dvrqapktorwspfrbbhbd.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2cnFhcGt0b3J3c3BmcmJiaGJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczOTc3MDUsImV4cCI6MjA2Mjk3MzcwNX0.lHeG2KXwQJfVmxoPC3GZg5JNB3I1ZbGJaJwZFosEdIQ'; // Use your anon or service role key here
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const db = createClient(supabaseUrl, supabaseKey);
 
 export default db;
